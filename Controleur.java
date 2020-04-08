7c6388768ce38eaacef4fe93f0fdb6c2dfd7f89f
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-07 23:34:40 -0400

    Update Controleur.java
    Vue et modele

--- Controleur.java
+++ Controleur.java
@@ -1,4 +1,13 @@
 public class Controleur {
+    
+    Vue vue;
+    Modele modele;
+
+    public Controleur(Vue vue){
+        this.vue = vue;
+        this.modele = new Modele();
+    }
+    
     //if "esc" Platform.exit
     //if "t" debug
 }
