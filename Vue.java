29d33e3a4a6783b9d2fc00d5e8cedffff41813be
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-07 23:44:45 -0400

    Update Vue.java

--- Vue.java
+++ Vue.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -13,30 +14,43 @@ import javafx.stage.Stage;
 
 public class Vue extends Application {
 
+    private Integer score;
+    private Meduse meduse;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.meduse = new Meduse();
+        this.score = 0;
+
         BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 350, 480, Color.MIDNIGHTBLUE);
 
-        Integer score = 0;
-        Text texteScore = new Text(score.toString());
-        texteScore.setFont(Font.font(25));
-        texteScore.setFill(Color.RED);
-
-        HBox scoreBox = new HBox();
-        scoreBox.setAlignment(Pos.CENTER);
-        scoreBox.getChildren().add(texteScore);
-        root.setTop(scoreBox);
-
+        Scene scene = new Scene(root, 350, 480, Color.BLUE);
         primaryStage.setScene(scene);
         primaryStage.setTitle("High Sea Tower");
         primaryStage.setResizable(false);
-        Image imgMeduse = new Image("/images/jellyfish1.png");
-        primaryStage.getIcons().add(imgMeduse);
+        Image logoMeduse = new Image("/images/jellyfish1.png");
+        primaryStage.getIcons().add(logoMeduse);
+
+        //Canvas
+
+        // Score
+        HBox scoreBox = new HBox();
+        scoreBox.setAlignment(Pos.CENTER);
+        Text texteScore = new Text(score.toString());
+        texteScore.setFont(Font.font(25));
+        texteScore.setFill(Color.RED);
+        scoreBox.getChildren().add(texteScore);
+        root.setTop(scoreBox);
+
+        // Méduse
+        Pane pane = new Pane();
+        Image imgMeduse = meduse.getImage();
+        pane.getChildren().add(imgMeduse);
+
 
         primaryStage.show();
     }
