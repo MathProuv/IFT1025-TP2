3de39ec92eb5833b0c052b96c94b01d53ed39d70
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-08 00:45:29 -0400

    Create Jeu.java

---
+++ Jeu.java
@@ -0,0 +1,10 @@
+import javafx.scene.canvas.GraphicsContext;
+
+public class Jeu {
+
+    public Jeu(){ }
+
+    public void update(double dt){ }
+
+    public void draw(GraphicsContext context){ };
+}
