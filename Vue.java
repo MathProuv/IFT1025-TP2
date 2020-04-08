448094a0c7342895130614e57460bf816f3c9592
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-08 00:42:07 -0400

    Update Vue.java
    canvas

--- Vue.java
+++ Vue.java
@@ -1,6 +1,10 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -13,9 +17,12 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Vue extends Application {
-
+    private int width = 350, height = 480;
+//*******************************
     private Integer score;
     private Meduse meduse;
+//*******************************
+    private Controleur controleur;
 
     public static void main(String[] args) {
         launch(args);
@@ -23,20 +30,67 @@ public class Vue extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //**************************
         this.meduse = new Meduse();
         this.score = 0;
+        //**************************
+        this.controleur = new Controleur(this);
 
         BorderPane root = new BorderPane();
 
-        Scene scene = new Scene(root, 350, 480, Color.BLUE);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("High Sea Tower");
-        primaryStage.setResizable(false);
-        Image logoMeduse = new Image("/images/jellyfish1.png");
-        primaryStage.getIcons().add(logoMeduse);
+        Scene scene = new Scene(root, width, height, Color.BLUE);
 
-        //Canvas
+        Canvas canvas = new Canvas(width, height);
+        root.getChildren().add(canvas);
 
+        Jeu jeu = new Jeu();
+
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()){
+                case T:
+                    controleur.deboguer();
+                    break;
+                case ESCAPE:
+                    Platform.exit();
+                    break;
+                case SPACE:
+                case UP:
+                    controleur.sauter();
+                    break;
+                case LEFT:
+                    controleur.bouger(false);
+                    break;
+                case RIGHT:
+                    controleur.bouger(true);
+                    break;
+            }
+        });
+
+        GraphicsContext context = canvas.getGraphicsContext2D();
+
+        AnimationTimer timer = new AnimationTimer() {
+            private long lastTime = 0;
+
+            @Override
+            public void handle(long now) {
+                if (lastTime == 0) {
+                    lastTime = now;
+                    return;
+                }
+
+                double dt = (now - lastTime) * 1e-9;
+
+                context.clearRect(0, 0, width, height);
+                jeu.update(dt);
+                jeu.draw(context);
+
+                lastTime = now;
+            }
+        };
+
+        timer.start();
+
+        // **********************************************
         // Score
         HBox scoreBox = new HBox();
         scoreBox.setAlignment(Pos.CENTER);
@@ -49,8 +103,14 @@ public class Vue extends Application {
         // Méduse
         Pane pane = new Pane();
         Image imgMeduse = meduse.getImage();
-        pane.getChildren().add(imgMeduse);
+        root.setCenter(pane);
+        // *********************************************
 
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("High Sea Tower");
+        primaryStage.setResizable(false);
+        Image logoMeduse = new Image("/images/jellyfish1.png");
+        primaryStage.getIcons().add(logoMeduse);
 
         primaryStage.show();
     }
