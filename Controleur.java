31e5a798dd700b033145cd4fc9507497dbc51479
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-09 14:17:50 -0400

    Update Controleur.java
    Interface !

--- Controleur.java
+++ Controleur.java
@@ -3,35 +3,18 @@ import javafx.scene.canvas.GraphicsContext;
 /**
  * @author Mathilde Prouvost et Augustine Poirier
  */
-public class Controleur {
-    Modele modele;
-    Jeu jeu;
+public interface Controleur {
 
-    public Controleur(){
-        this.modele = new Modele();
-        this.jeu = new Jeu();
-    }
+    public Integer getScore();
 
-    public void update(double dt, double deltaT){
-        jeu.update(dt, deltaT);
-    }
-    public void draw(GraphicsContext context){
-        jeu.draw(context);
-    }
+    public void update(double dt, double deltaT);
+    public void draw(GraphicsContext context);
 
-    public void deboguer(){
-        System.out.println("debug " + jeu.isScroll());
-        jeu.changeScroll();
-    }
+    public void deboguer();
 
-    public void sauter(){
-        System.out.println("Saute !");
-    }
+    public void sauter();
+    public void stopSaut();
 
-    public void bouger(boolean direction){
-        if (direction)
-            System.out.println("droite");
-        else
-            System.out.println("gauche");
-    }
+    public void tourner(boolean direction);
+    public void stopTourn();
 }
