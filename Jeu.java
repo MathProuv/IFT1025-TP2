6b6c003d4db61246a0bd03650b9ed4cccebdcef0
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-09 14:18:26 -0400

    Update Jeu.java
    implement Controleur

--- Jeu.java
+++ Jeu.java
@@ -3,17 +3,13 @@ import javafx.scene.canvas.GraphicsContext;
 /**
  * @author Mathilde Prouvost et Augustine Poirier
  */
-public class Jeu {
+public class Jeu implements Controleur {
     private Meduse meduse;
     private int score;
     private boolean scroll; //false avant le debut de la partie et en mode debug
 
     public void changeScroll(){
         this.scroll = !this.scroll;
-    }
-
-    public boolean isScroll() {
-        return scroll;
     }
 
     public Integer getScore() {
@@ -33,4 +29,27 @@ public class Jeu {
     public void draw(GraphicsContext context){
         meduse.draw(context, score);
     };
+
+
+    public void deboguer(){
+        System.out.println("debug " + this.scroll);
+        this.changeScroll();
+    }
+
+    public void sauter(){
+        System.out.println("Saute !");
+    }
+    public void stopSaut(){}
+
+    public void tourner(boolean direction){
+        if (direction)
+            System.out.println("droite");
+        else
+            System.out.println("gauche");
+        meduse.tourner(direction);
+    }
+
+    public void stopTourn(){
+        meduse.stopTourn();
+    }
 }
