26d22f41942c086f390a06f285b6235a4c41277e
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-09 00:16:06 -0400

    Update Jeu.java
    avec une méduse

--- Jeu.java
+++ Jeu.java
@@ -1,10 +1,15 @@
 import javafx.scene.canvas.GraphicsContext;
 
 public class Jeu {
+    private Meduse meduse;
 
-    public Jeu(){ }
+    public Jeu(){
+        this.meduse = new Meduse();
+    }
 
     public void update(double dt){ }
 
-    public void draw(GraphicsContext context){ };
+    public void draw(GraphicsContext context){
+        meduse.draw(context);
+    };
 }
