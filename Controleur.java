44371000e615409971649587112eca8a414c6857
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-09 11:58:42 -0400

    Update Controleur.java

--- Controleur.java
+++ Controleur.java
@@ -1,19 +1,33 @@
-public class Controleur {
-    Vue vue;
-    Modele modele;
+import javafx.scene.canvas.GraphicsContext;
 
-    public Controleur(Vue vue){
-        this.vue = vue;
+/**
+ * @author Mathilde Prouvost et Augustine Poirier
+ */
+public class Controleur {
+    Modele modele;
+    Jeu jeu;
+
+    public Controleur(){
         this.modele = new Modele();
+        this.jeu = new Jeu();
+    }
+
+    public void update(double dt, double deltaT){
+        jeu.update(dt, deltaT);
+    }
+    public void draw(GraphicsContext context){
+        jeu.draw(context);
     }
 
     public void deboguer(){
-        System.out.println("debug");
+        System.out.println("debug " + jeu.isScroll());
+        jeu.changeScroll();
     }
 
     public void sauter(){
         System.out.println("Saute !");
     }
+
     public void bouger(boolean direction){
         if (direction)
             System.out.println("droite");
