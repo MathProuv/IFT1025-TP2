ffd7a4f7308b533717029846ea86ec10ee61bab3
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-09 21:41:08 -0400

    Update Controleur.java

--- Controleur.java
+++ Controleur.java
@@ -3,18 +3,54 @@ import javafx.scene.canvas.GraphicsContext;
 /**
  * @author Mathilde Prouvost et Augustine Poirier
  */
-public interface Controleur {
+public class Controleur  {
+    private Modele modele = new Modele();
+    private Meduse meduse;
+    private int score;
+    private boolean commence; //false avant le debut de la partie et en mode debug
+    private boolean debug;
 
-    public Integer getScore();
+    public void changeDebug(){
+        this.debug = !this.debug;
+    }
 
-    public void update(double dt, double deltaT);
-    public void draw(GraphicsContext context);
+    public Integer getScore() {
+        return score;
+    }
 
-    public void deboguer();
+    public Controleur(){
+        this.meduse = new Meduse();
+        this.score = 0;
+        this.commence = false;
+        this.debug = false;
+    }
 
-    public void sauter();
-    public void stopSaut();
+    public void update(double dt, double deltaT){
+        meduse.update(dt, deltaT);
+    }
 
-    public void tourner(boolean direction);
-    public void stopTourn();
+    public void draw(GraphicsContext context){
+        meduse.draw(context, score);
+    };
+
+    public void deboguer(){
+        System.out.println("debug " + debug);
+        this.changeDebug();
+    }
+
+    public void sauter(){
+        modele.faireSauter(meduse);
+    }
+
+    public void tourner(boolean direction){
+        if (direction)
+            System.out.println("droite");
+        else
+            System.out.println("gauche");
+        meduse.tourner(direction);
+    }
+
+    public void stopTourn(){
+        meduse.stopTourn();
+    }
 }
