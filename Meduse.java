f9ef7d87a2d64bfdf0bf8b830243829f92573ad3
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-10 16:21:27 -0400

    Update Meduse.java

--- Meduse.java
+++ Meduse.java
@@ -1,39 +1,105 @@
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
-public class Modele {
+/**
+ *@author Mathilde Prouvost et Augustine Poirier
+ */
+public class Meduse{
+    private Image image;
+    private boolean direction;
+    private double x, y, vx, vy, ax, ay;
+
+    private double coefFrotY = 1;
+    private double coefFrotX = 5;
+
     private static int widthFenetre = 350, heightFenetre = 480;
     private static int tailleMeduse = 50;
 
+    private static double frameRate = 8 * 1e-9;
+    private static Image[] framesDroite = new Image[]{
+            new Image("/images/jellyfish1.png"),
+            new Image("/images/jellyfish2.png"),
+            new Image("/images/jellyfish3.png"),
+            new Image("/images/jellyfish4.png"),
+            new Image("/images/jellyfish5.png"),
+            new Image("/images/jellyfish6.png")
+    };
+    private static Image[] framesGauche = new Image[]{
+            new Image("/images/jellyfish1g.png"),
+            new Image("/images/jellyfish2g.png"),
+            new Image("/images/jellyfish3g.png"),
+            new Image("/images/jellyfish4g.png"),
+            new Image("/images/jellyfish5g.png"),
+            new Image("/images/jellyfish6g.png")
+    };
 
-    public Modele(int width, int height) {
-        this.widthFenetre = width;
-        this.heightFenetre = height;
+    /**
+     * Constructeur
+     */
+    public Meduse(){
+        this.direction = true;
+        this.image = framesDroite[0];
+        this.x = (double)(widthFenetre-tailleMeduse)/2; //cast facultatif
+        this.y = 0;
+        this.vx = 0;
+        this.vy = 0;
+        this.ax = 0;
+        this.ay = -1200;
     }
 
-    public void update(double dt, double deltaT, Meduse meduse, Integer score) {
-        meduse.update(dt, deltaT);
-        if (isInWall(meduse)) {
-            hitWall(meduse);
+    public void update(double dt, double deltaT){
+        int frame = (int) (frameRate*deltaT);
+        if (direction)
+            this.image = framesDroite[frame%framesDroite.length];
+        else
+            this.image = framesGauche[frame%framesGauche.length];
+
+        this.vx += ax * dt - coefFrotX*vx*dt;
+        this.vy += ay * dt - coefFrotY*vy*dt;
+        this.x += vx * dt;
+        this.y += vy * dt;
+
+        // à enlever pour mourir
+        if (this.y<0)
+            this.y = 0; //partie pas encore commencée
+    }
+
+    public void draw(GraphicsContext context, double score){
+        double yFenetre = heightFenetre - tailleMeduse - this.y + score;
+        context.clearRect(x, yFenetre, tailleMeduse, tailleMeduse);
+        context.drawImage(image, x, yFenetre, tailleMeduse, tailleMeduse);
+    }
+
+    public void sauter(){
+        this.vy = 600;
+    }
+
+    public void rebondir() {
+        this.vx *= -1;
+        this.direction = !this.direction;
+
+        if (this.x <= 0) {
+            this.x *= -1;
+        } else {
+            this.x = 2*widthFenetre - this.x - 2*tailleMeduse ;
         }
     }
 
-    public void draw(GraphicsContext context, Meduse meduse, Integer score) {
-        meduse.draw(context, score);
+    public void tourner(boolean direction){
+        if (direction != this.direction) {
+            this.direction = direction;
+        }
+        ax = direction ? 1200 : -1200;
+    }
+    public void stopTourn(){
+        ax = 0;
     }
 
-    public void sauter (Meduse meduse) {
-        meduse.sauter();
+    public double getX() {
+        return x;
     }
 
-    public void tourner(Meduse meduse, boolean direction) {
-        meduse.tourner(direction);
-    }
-
-    public boolean isInWall(Meduse meduse) {
-        return ((meduse.getX() <= 0) || (meduse.getX() >= widthFenetre - tailleMeduse));
-    }
-
-    public void hitWall(Meduse meduse) {
-        meduse.rebondir();
+    public double getY() {
+        return y;
     }
 }
