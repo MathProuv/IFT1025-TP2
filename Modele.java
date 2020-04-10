fdcc7671f85633abf860ed4bd00eebaa45a449c1
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-09 21:42:13 -0400

    Update Modele.java

--- Modele.java
+++ Modele.java
@@ -1,6 +1,15 @@
-/**
- * @author Mathilde Prouvost et Augustine Poirier
- */
 public class Modele {
+    private static int widthFenetre = 350, heightFenetre = 480;
+    private static int tailleMeduse = 50;
+
+
     public Modele(){ }
+
+    public void faireSauter (Meduse meduse){
+        meduse.sauter();
+    }
+
+    public void tourner(Meduse meduse, boolean direction){
+
+    }
 }
