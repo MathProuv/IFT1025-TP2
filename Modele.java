6c7fe268203c80866920c98dc54ed6351696d374
inkbloot <63125080+inkbloot@users.noreply.github.com>
2020-04-10 19:12:40 -0400

    Update Modele.java

--- Modele.java
+++ Modele.java
@@ -1,5 +1,8 @@
 import javafx.scene.canvas.GraphicsContext;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 public class Modele {
     private static int widthFenetre = 350, heightFenetre = 480;
     private static int tailleMeduse = 50;
@@ -17,8 +20,11 @@ public class Modele {
         }
     }
 
-    public void draw(GraphicsContext context, Meduse meduse, Integer score) {
+    public void draw(GraphicsContext context, Meduse meduse, Integer score, ArrayList<Plateforme> plateformes) {
         meduse.draw(context, score);
+        for (Plateforme plat : plateformes) {
+            plat.draw(context, score);
+        }
     }
 
     public void sauter (Meduse meduse) {
@@ -34,6 +40,35 @@ public class Modele {
     }
 
     public void hitWall(Meduse meduse) {
-        meduse.rebondir();
+        meduse.rebondirMur();
+    }
+
+
+    public boolean intersectsPlateforme(Meduse meduse, Plateforme plateforme) {
+        boolean intersectOver = (meduse.getY() - plateforme.getY() < 5) //méduse et plateforme sont à la même hauteur
+                && (meduse.getVy() <= 0) //méduse tombe ou ne bouge pas
+                && (plateforme.getX() - plateforme.getLargeur()/2 <= meduse.getX())
+                && (meduse.getX() >= plateforme.getX() + plateforme.getLargeur()/2); //méduse qqpart sur la plateforme
+
+        if (!(plateforme instanceof PlateformeSolide))
+            return intersectOver;
+
+        boolean intersectUnder = (meduse.getY() - plateforme.getY()+plateforme.getHauteur() < 5) //méduse et plateforme sont à la même hauteur
+                && (meduse.getVy() > 0)
+                && (plateforme.getX() - plateforme.getLargeur()/2 <= meduse.getX())
+                && (meduse.getX() >= plateforme.getX() + plateforme.getLargeur()/2); //méduse qqpart sur la plateforme
+
+        return intersectUnder || intersectOver;
+    }
+
+    public void hitPlateforme(Meduse meduse, Plateforme plateforme) {
+        if (plateforme instanceof PlateformeSimple)
+            ((PlateformeSimple) plateforme).hitPlateforme(meduse);
+        if (plateforme instanceof PlateformeRebondissante)
+            ((PlateformeRebondissante) plateforme).hitPlateforme(meduse);
+        if (plateforme instanceof PlateformeSolide)
+            ((PlateformeSolide) plateforme).hitPlateforme(meduse);
+        if (plateforme instanceof PlateformeAccelerante)
+            ((PlateformeAccelerante) plateforme).hitPlateforme(meduse);
     }
 }
