9384ed0c342ea5e92dd1c930f691238f6a3d5666
Mathilde P <62081299+MathProuv@users.noreply.github.com>
2020-04-10 16:16:06 -0400

    Update Controleur.java

--- Controleur.java
+++ Controleur.java
@@ -1,4 +1,10 @@
+import javafx.geometry.Pos;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  * @author Mathilde Prouvost et Augustine Poirier
@@ -7,11 +13,12 @@ public class Controleur  {
 
     Jeu jeu;
     Modele modele;
+    int width = 350;
 
 
     public Controleur(){
-       this.jeu = new Jeu();
-       this.modele = new Modele();
+        this.jeu = new Jeu();
+        this.modele = jeu.getModele();
     }
 
     public void update(double dt, double deltaT){
@@ -20,22 +27,25 @@ public class Controleur  {
 
     public void draw(GraphicsContext context){
         jeu.draw(context);
+        context.setFont(new Font(25));
+        context.setFill(Color.RED);
+        context.fillText(getScore().toString() + " m", width/2, 25);
     };
 
     public void deboguer(){
-        //System.out.println("debug " + jeu.isDebug());
+        System.out.println("debug " + jeu.isDebug());
         jeu.changeDebug();
     }
 
     public void sauter(){
-       jeu.sauter();
+        jeu.sauter();
     }
 
     public void tourner(boolean direction){
         //if (direction)
-           //System.out.println("droite");
+        //System.out.println("droite");
         //else
-           //System.out.println("gauche");
+        //System.out.println("gauche");
         jeu.tourner(direction);
     }
 
